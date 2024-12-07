6b985a74e060e738ee3cd673a78b34406da2031a
melindapozna <melindapozna@gmail.com>
2024-12-07 23:26:09 +0200

    Add obstacles to game, expand character collision sensor to sense obstacles too

--- src/objects/obstacle.py
+++ src/objects/obstacle.py
@@ -1,9 +1,8 @@
-import random
-
-
 class Obstacle:
     def __init__(self, id, radius, pos):
         self.id = id
         self.radius = radius
         self.pos = pos
 
+    def accept(self, visitor):
+        return visitor.visit_obstacle(self)
