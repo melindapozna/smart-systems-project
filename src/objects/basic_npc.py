24cd7dcf45b2bf704552dc2f2542b002827df35c
Andrii-Solo <157142750+Andrii-Solo@users.noreply.github.com>
2024-12-08 11:30:22 +0200

    Add files via upload
    Smooth camera movement. 
    NPC don't come too close to the player.
    If player come too close to npc, it goes back.

--- src/objects/basic_npc.py
+++ src/objects/basic_npc.py
@@ -1,43 +1,29 @@
 from pygame import Vector2
-from src.objects.bullet import Bullet
-from src.object_visitors.collisions.basic_npc_collision_visitor import BasicNPCCollisionVisitor
+from objects.bullet import Bullet
 import time
 import math
 import random
 
-
 class BasicNPC:
-    def __init__(self, id, x, y, speed, player_sensor, border_sensor, character_collision_sensor, game_stats):
-        self.id = id
-        self.radius = 10
+    def __init__(self, x, y, speed, player_sensor, border_sensor):
         self.pos = Vector2(x, y)
         self.speed = speed
         self.dir = Vector2(0, 1)
         self.player_sensor = player_sensor
         self.border_sensor = border_sensor
-        self.character_collision_sensor = character_collision_sensor
         self.hp = 50
         self.alive = True
-        self.damage = 10
-        self.bullet_radius = 2
+        self.damage = 20
         self.bullet_ready = False
         self.prev_shot_time = time.time()
-        self.collided = False
-        self.prev_collision_time = time.time()
-        # Directions in which the NPC can't move
-        self.constraints = []
-        self.items = []
-        self.collision_visitor = BasicNPCCollisionVisitor(self)
-        self.fire_rate = 1
-        self.game_stats = game_stats
-        self.vision_radius = 400
-        self.vision_angle = 40
-        self.last_known_location = None
-        self.last_check = time.time()
-
+        self.vision_radius= 400
+        self.lastKnownLocation = None
+        self.lastCheck = time.time()
+        self.newSightDir = None
+        self.rotateCounter = 0
     def action(self, dt):
-        b_Point = self.pos + self.vision_radius * self.dir.rotate(20)
-        c_Point = self.pos + self.vision_radius * self.dir.rotate(-20)
+        b_Point = self.pos + self.vision_radius*self.dir.rotate(20)
+        c_Point = self.pos + self.vision_radius*self.dir.rotate(-20)
         character_position = self.player_sensor.get_reading()
         side_1 = self.pos.distance_to(b_Point)
         side_2 = self.pos.distance_to(c_Point)
@@ -49,55 +35,50 @@ class BasicNPC:
         p2 = (side_1 + side_4 + side_5) / 2
         p3 = (side_3 + side_5 + side_6) / 2
         p4 = (side_2 + side_4 + side_6) / 2
-        area1 = math.sqrt(p1 * (p1 - side_1) * (p1 - side_2) * (p1 - side_3))
-        area2 = math.sqrt(p2 * (p2 - side_1) * (p2 - side_4) * (p2 - side_5))
-        area3 = math.sqrt(p3 * (p3 - side_3) * (p3 - side_5) * (p3 - side_6))
-        area4 = math.sqrt(p4 * (p4 - side_2) * (p4 - side_4) * (p4 - side_6))
-        if area2 + area3 + area4 + 1 >= area1 >= area2 + area3 + area4 - 1:
+        area1 = math.sqrt(p1*(p1 - side_1)*(p1 - side_2) * (p1 - side_3))
+        area2 = math.sqrt(p2*(p2 - side_1)*(p2 - side_4) * (p2 - side_5))
+        area3 = math.sqrt(p3*(p3 - side_3)*(p3 - side_5) * (p3 - side_6))
+        area4 = math.sqrt(p4*(p4 - side_2)*(p4 - side_4) * (p4 - side_6))
+        if(area1 <= area2 + area3 + area4 + 10 and area1 >= area2 + area3 + area4 - 10):
             self.move(dt)
-            self.last_known_location = character_position.copy()
+            self.lastKnownLocation = character_position.copy()
         else:
             self.searching(dt)
-
+    
     def searching(self, dt):
-        if self.last_known_location is not None:
-            self.look_at(self.last_known_location)
+        if(self.lastKnownLocation != None):
+            self.look_at(self.lastKnownLocation)
             self.pos += self.speed * dt * self.dir
-            if self.last_known_location.distance_to(self.pos) < 2:
-                self.last_known_location = None
+            if(self.pos.x > self.lastKnownLocation.x - 1 and self.pos.x < self.lastKnownLocation.x + 1 and
+               self.pos.y > self.lastKnownLocation.y -1 and self.pos.y < self.lastKnownLocation.y + 1):
+                self.lastKnownLocation = None
         else:
-            if time.time() - self.last_check > 3:
-                self.dir = self.dir.rotate(random.randint(0, 180))
-                self.last_check = time.time()
-
-    def add_constraint(self, constraint):
-        self.constraints.append(constraint)
-
-    def process_constraints(self, speed_vector):
-        for constraint in self.constraints:
-            normal_component = speed_vector.dot(constraint)
-            if normal_component > 0:
-                speed_vector -= normal_component * constraint
-        self.constraints = []
-        return speed_vector
+            if(time.time() - self.lastCheck > 3 and self.newSightDir == None):
+                self.newSightDir = random.randint(0, 180)
+                self.lastCheck = time.time()
+            elif(self.newSightDir != None and time.time() - self.lastCheck):
+                if(time.time() - self.lastCheck > 0.01):
+                    self.dir = self.dir.rotate(1)
+                    self.rotateCounter += 1
+                    self.lastCheck = time.time()
+                    if(self.rotateCounter >= self.newSightDir):
+                        self.newSightDir = None
+                        self.rotateCounter = 0
+                
 
     def move(self, dt):
-        colliding_objects = self.character_collision_sensor.get_reading(self)
-        if colliding_objects:
-            for colliding_object in colliding_objects:
-                colliding_object.accept(self.collision_visitor)
-
         self.look_at(self.player_sensor.get_reading())
-        speed_vector = self.speed * self.dir
-        speed_vector = self.process_constraints(speed_vector)
-        self.pos += dt * speed_vector
+        if(self.pos.distance_to(self.player_sensor.get_reading())>60):
+            self.pos += self.speed * dt * self.dir
+        elif(self.pos.distance_to(self.player_sensor.get_reading())<40):
+            self.pos -= self.speed * dt * self.dir
 
-        # Shoot a bullet if the previous shot was at least 1 second ago
-        if time.time() - self.prev_shot_time > self.fire_rate:
+        # Shoot a bullet if a second has passed since the last one
+        if not self.bullet_ready and time.time() - self.prev_shot_time > 1:
             self.bullet_ready = True
 
         # Kill the NPC if it reaches the border
-        if self.border_sensor.get_reading(self):
+        if self.border_sensor.get_reading(self.pos):
             self.alive = False
 
     def look_at(self, position):
@@ -111,17 +92,11 @@ class BasicNPC:
         if self.hp <= 0:
             self.alive = False
 
-    def shoot_bullet(self, collision_sensor, bullet_id):
+    def shoot_bullet(self, offset, collision_sensor):
         self.bullet_ready = False
         self.prev_shot_time = time.time()
-        # offset: radius of the shooter to avoid bullet collision with the shooter itself
-        offset = self.radius + self.bullet_radius
-        bullet_pos = self.pos + offset * self.dir
-        self.game_stats.track_bullet_fired()
-        return Bullet(bullet_pos, self.dir, self.damage, self.bullet_radius, collision_sensor, bullet_id)
 
-    def pick_up(self, item):
-        self.items.append(item)
+        return Bullet(self.pos + offset * self.dir, self.dir, self.damage, collision_sensor)
 
     def accept(self, visitor):
         return visitor.visit_basic_npc(self)
